ddb161720a5a26f8177c4d1aaaa3dbfcc3565a7f
Shivendra <shivendra@ipropal.com>
2019-06-12 12:21:34 +0530

    ADD button row in editor

--- App/js/editor.js
+++ App/js/editor.js
@@ -5,3 +5,22 @@ CKEDITOR.inline('editor');
 
 var data = CKEDITOR.instances.editor.getData();
 console.log(data);
+
+
+/**
+ * Button row 
+ */
+var i = 0;
+var starIcon = document.getElementById("starIcon")
+starIcon.style.cursor = 'pointer';
+starIcon.onclick = function() {
+    if (i % 2 == 0) {
+        starIcon.classList.remove('glyphicon-star-empty');
+        starIcon.classList.add('glyphicon-star');
+        i++
+    } else {
+        starIcon.classList.remove('glyphicon-star');
+        starIcon.classList.add('glyphicon-star-empty');
+        i++
+    }
+};
